59ebfa764953b2335806536237e2355d365b9867
Ishimoto Koji <koji.ishimoto@gmail.com>
2025-10-06 15:55:48 +0900

    紅色の追加

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,13 +1,11 @@
-import { Image } from "minista"
+// import { Image } from "minista"
 
 export function Hero() {
   return (
     <div className="w-full h-screen">
       <div className="size-full overflow-hidden grid place-items-center">
-        <Image
-          alt="表紙画像"
-          src="/src/assets/images/dummy.webp"
-          className="absolute inset-0 -z-10 size-full object-cover"
+        <div
+          className="absolute inset-0 -z-10 size-full object-cover bg-beni"
         />
         <div className="text-center">
           <h1 className="text-5xl font-semibold tracking-tight text-balance">
