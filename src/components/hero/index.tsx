7f1a6df9d51052039f8d609861ac254f01f1fa5c
Ishimoto Koji <koji.ishimoto@gmail.com>
2025-10-07 01:17:17 +0900

    ヒーローイメージの調整

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -2,12 +2,12 @@
 
 export function Hero() {
   return (
-    <div className="w-full h-screen isolate">
+    <div className="w-full h-[90svh] isolate">
       <div className="relative size-full overflow-hidden grid place-items-center">
         <div
           className="absolute inset-0 -z-10 size-full object-cover bg-beni"
         />
-        <div className="text-center">
+        <div className="absolute left-8 sm:left-12 md:left-16 top-12 sm:top-20 md:top-24">
           <h1 className="text-5xl font-semibold tracking-widest text-balance font-serif text-gray-50 drop-shadow-lg vertical-rl">
             かんちこんち
           </h1>
