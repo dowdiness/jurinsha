119f603808754511a0e7ef531e4cbf63fa7a8718
Ishimoto Koji <koji.ishimoto@gmail.com>
2025-10-15 13:50:33 +0900

    絵本の画像を追加してデザインを修正

--- src/components/character/index.tsx
+++ src/components/character/index.tsx
@@ -3,12 +3,12 @@ import { characters } from "./character"
 export function Character() {
   return (
     <div className="w-full px-8 sm:px-12 md:px-16 lg:px-32 xl:px-48 2xl:px-64 pt-12 pb-24 bg-beige text-gray-900">
-      <h2 className="text-3xl font-medium font-rounded mb-8">キャラクター紹介</h2>
+      <h2 className="text-3xl font-medium mb-8">キャラクター紹介</h2>
       <div className="space-y-6">
         {Object.entries(characters).map(([key, value]) => (
           <div key={key}>
-            <h3 className="text-xl md:text-2xl font-rounded font-medium mb-2">{key}</h3>
-            <p className="text-lg md:text-xl leading-relaxed font-serif">{value}</p>
+            <h3 className="text-xl md:text-2xl font-medium mb-4">{key}</h3>
+            <p className="text-lg md:text-xl leading-relaxed">{value}</p>
           </div>
         ))}
       </div>
--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,15 +1,15 @@
+import { Image } from "minista"
+
 export function Hero() {
   return (
     <div className="w-full h-[90svh] isolate bg-beni">
-      <div className="relative h-full overflow-hidden grid place-items-center mx-6 sm:mx-12 md:mx-16 lg:mx-32 xl:mx-48 2xl:mx-64">
+      <div className="relative h-full grid place-items-center mx-6 sm:mx-12 md:mx-16 lg:mx-32 xl:mx-48 2xl:mx-64">
         <div className="absolute left-0 top-12 sm:top-20 md:top-24">
           <h1 className="text-5xl tracking-widest text-balance text-gray-50 drop-shadow-lg vertical-rl">
             かんちこんち
           </h1>
         </div>
-        <div className="absolute right-8 bg-gray-800 w-32 md:w-64 h-[60%] md:h-[80%] text-gray-50">
-          綾傘鉾
-        </div>
+        <Image src="/src/assets/images/ayakasahoko.png" className="absolute right-0 w-full object-contain object-top-right h-full -mr-6 sm:-mr-12 md:-mr-16 lg:-mr-32 xl:-mr-48 2xl:-mr-64" aspect="1.414:1" width="800" height="1120" />
       </div>
     </div>
   )
