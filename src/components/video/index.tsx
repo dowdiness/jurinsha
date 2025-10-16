479069a982190d9334ebd55925a442d530e315e9
Ishimoto Koji <koji.ishimoto@gmail.com>
2025-10-16 20:51:06 +0900

    動画の置き場所をpublicに変更

--- src/components/video/index.tsx
+++ src/components/video/index.tsx
@@ -3,7 +3,7 @@ export function Video() {
   <div className="w-full px-6 sm:px-12 md:px-16 lg:px-32 xl:px-48 2xl:px-64 pb-24">
     <h2 className="text-3xl font-medium mb-8 md:mb-16">メイキング動画</h2>
     <video controls>
-      <source src="/src/assets/making.mp4" type="video/mp4" />
+      <source src="/making.mp4" type="video/mp4" />
     </video>
   </div>
   )
